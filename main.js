2379fd81d15ecd6bcbc379b025b146e5c19ae4a8
qweqwr@gmail.com <bakdildora3@gmail.com>
2024-08-29 11:06:49 +0500

    save

---
+++ main.js
@@ -0,0 +1,7 @@
+var Name = prompt('Введите ваше имя')
+var year1 = +prompt('Введите нынешний год')
+var year2 = +prompt('Введите ваш год рождения ')
+
+
+
+console.log(Name + ', Ваш возраст ' + (year1 - year2));
